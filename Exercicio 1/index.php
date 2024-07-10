b354bdd354e120606c8b52062bd4a4e26fad79ac
Sergio <sergio.htp7@gmail.com>
2024-07-10 17:02:42 -0300

    Exercicio PHP

--- Exercicio 1/index.php
+++ Exercicio 1/index.php
@@ -1,1 +1,31 @@
-<?php ?>
+<?php 
+
+//Q1
+$banda = 'Iron Maiden';
+echo $banda;
+echo "<br>";
+echo "<br>";
+
+//Q2
+$nome = 'Edlan';
+$idade = 27;
+$endereco = 'Rua Telmo Mendes';
+$curso = 'Design/Programador Web';
+
+echo "Olá eu sou $nome, tenho $idade anos, moro em $endereco e faço o curso de $curso no SENAC";
+echo "<br>";
+echo "<br>";
+
+//Q9
+$alunos = array(
+    'Junior' => 9.5,
+    'Maria' => 10,
+    'Paulo' => 6,
+    'Ana' => 8.5,
+    'Pedro' => 5.5,
+    'Julia' =>6.5
+);
+
+function aprovados($alunos)
+
+?>
