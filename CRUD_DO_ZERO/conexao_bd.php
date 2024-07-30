1f938bbce6610ae3714673d0a7a614a787da3e69
DEV-EdCS <edlan.carvalho15@gmail.com>
2024-07-30 10:06:23 -0300

    aprendendo crud passo a passo

---
+++ CRUD_DO_ZERO/conexao_bd.php
@@ -0,0 +1,16 @@
+<?php 
+
+// Configurações de conexão
+$servidor_banco = "localhost"; // Cria uma variável no php para se comunicar com o servidor local do banco de dados
+$usuario_banco = "root"; // Para exemplo vamos usar o usuário administrador padrão "root" do MySQL
+$senha_banco = ""; // Senha do usuário root (vazia no ambiente de desenvolvimento)
+$nome_banco = "banco_dados"; // Nome do banco de dados que foi criado
+
+// Criar conexão com o banco de dados
+$conexao_banco = new mysqli($servidor_banco, $usuario_banco, $senha_banco, $nome_banco); // Cria uma variável que se comunica com a função mysqli(cria uma nova conexão com o banco de dados usando os parâmetros fornecidos)
+
+// Cria uma função para verifica se houve erro na conexão
+if ($conexao_banco->connect_error) { // Se houver, exibe uma mensagem de erro e interrompe a execução do script com 'die'
+    die("Falha na conexão com o banco de dados: " . $conexao_banco->connect_error);
+}
+?>
