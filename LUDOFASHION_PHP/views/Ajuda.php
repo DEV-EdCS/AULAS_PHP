82a08797b06bcd801029534fba7c4c28cf327fb3
Sergio <sergio.htp7@gmail.com>
2024-08-14 17:49:22 -0300

    Finalizando as paginas

---
+++ LUDOFASHION_PHP/views/Ajuda.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/Ajuda.css">
+    <title>LudoFashion - Ajuda</title>
+</head>
+
+<body>
+
+<?php include 'header.php'; ?>
+
+    <nav>
+
+        <a href="">Catálogo</a>
+
+        <a href="">Sobre a Loja</a>
+
+    </nav>
+    <div>
+        <h1>Principais perguntas:</h1>
+    </div>
+    <section class="conteudo">
+        <div class="infos">
+
+            <details class="detalhes">
+                <summary>Como faço para adicionar um produto à minha lista de desejos?</summary>
+                <p class="perguntas">Após selecionar seu produto escolhido, clique no coração ao lado da imagem.</p>
+            </details>
+
+            <details class="detalhes">
+                <summary >Como posso pesquisar produtos no site?</summary>
+                <p class="perguntas">Através da barra de busca ou por meio da seleção por categorias.</p>
+            </details>
+            <details class="detalhes">
+                <summary >Existe uma opção de filtragem para encontrar produtos específicos, como por categoria, preço, marca, etc?</summary>
+                <p class="perguntas">Existe sim, inclusive esse recurso facilita a busca por novos produtos.</p>
+            </details>
+            <details class="detalhes">
+                <summary>Posso salvar produtos em minha lista de desejos para ver mais tarde?
+                </summary>
+                <p class="perguntas">Pode sim, acessando seu perfil você consegue visualizar toda sua lista de
+                    desejos.</p>
+            </details>
+            <details class="detalhes">
+                <summary>Existe uma opção de chat ao vivo ou suporte online para obter ajuda adicional
+                    sobre o produto?</summary>
+                <p class="perguntas">É claro, ao lado você acessa o nosso canal de atendimento.</p>
+            </details>
+        </div>
+        <div class="infos2">
+            <img class="img-redonda" src="../images/Won-duv.jpg" alt="" width="400px">
+            <a class="fconosco" href="">FALE CONOSCO!<img src="../images/atendimento-ao-cliente.png" width="50px"  height30px=""></a>
+        </div>
+    </section>
+    <?php include 'footer.php'; ?>
+</body>
+
+</html>
---
+++ LUDOFASHION_PHP/views/Busca.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Busca - LudoFashion</title>
+    <link rel="stylesheet" href="../css/Busca.css">
+</head>
+
+<?php include 'header.php'; ?>
+
+<nav>
+
+    <a href="">Catálogo</a>
+
+    <a href="">Sobre a Loja</a>
+
+</nav>
+
+<body>
+    <main class="conteudo">
+        <section class="part1">
+            <h1>Categorias</h1>
+            <div class="checklist">
+                <input type="checkbox"> Blusa <br>
+                <input type="checkbox"> Saia <br>
+                <input type="checkbox"> Vestido <br>
+                <input type="checkbox"> Lingerie <br>
+                <input type="checkbox"> Maquiagem <br>
+                <input type="checkbox"> Bolsa <br>
+            </div>
+        </section>
+        <section>
+            <div class="titulo1">
+                <h2>Conheça nosso catálogo de produtos</h2>
+                <p>Os mais variados produtos femininos, com os melhores preços de mercado!</p>
+            </div>
+            <div class="part2">
+                
+            <div class="card-container">
+                <div class="card">
+                    <div class="card-img">
+                        <img src="../images/BLUSA-MC-BORDADO-CAJU-OFF-WHITE-P-10056506013-C1.webp" alt="" width="100%">
+                    </div>
+                    <div class="card-content">
+                        <p>Blusa Feminina Estampa Caju Manga Curta</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="../images/BLUSA-MC-LASTEX-MADALENA-ESTAMP-PTO-48-10056350876-C1.webp" alt="" width="100%">
+                    </div>
+                    <div class="card-content">
+                        <p>Blusa Plus Size Feminina Floral Lastex Marisa</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="../images/BLUSA-MC-STICTH-VERDE-VDE-P-10056295689-C1.webp" alt="" width="100%">
+                    </div>
+                    <div class="card-content">
+                        <p>Blusa Feminina Stitch Manga Curta Disney</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="../images/Blusa-Regata-Feminina-Canelada-Gola-Alta-Marisa-10056515213-C1.webp" alt="" width="100%">
+                    </div>
+                    <div class="card-content">
+                        <p>Blusa Regata Feminina Canelada Gola Alta</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="../images/CALCA-DE-MOLETOM-COM-VINCO-AZL-P-10055218481-C1.webp" alt="" width="100%">
+                    </div>
+                    <div class="card-content">
+                        <p>Calça Moletom Reta Feminina</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="../images/Calca-Feminina-Reta-Alfaiataria-Risca-De-Giz-Marisa-10056521207-C1.webp" alt="" width="100%">
+                    </div>
+                    <div class="card-content">
+                        <p>Calça Feminina Reta Alfaiataria Risca De Giz</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="../images/CALCA-JNS-FLARE-BOTOES-PLUS-JES-48-10055783439-C1.webp" alt="" width="100%">
+                    </div>
+                    <div class="card-content">
+                        <p>Calça Plus Size Feminina Jeans Flare</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="../images/CALCA-WIDE-ALFAIATADA-PRETA-PTO-P-10056298529-C1.webp" alt="" width="100%">
+                    </div>
+                    <div class="card-content">
+                        <p>Calça Wide Leg Feminina Cintura Alta Bolsos</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="../images/Camisa-Feminina-Jeans-Bolsos-Manga-Longa-Marisa-10050567829-C1.webp" alt="" width="100%">
+                    </div>
+                    <div class="card-content">
+                        <p>Camisa Feminina Jeans Bolsos Manga Longa</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="../images/VESTIDO-CHEMISE-MANGA-CURTA-EST-Mrm-38-10055209397-C1.webp" alt="" width="100%">
+                    </div>
+                    <div class="card-content">
+                        <p>Vestido Feminino Chemise Estampa Poá</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="../images/VESTIDO-JEANS-MANGA-TRES-QUARTO-JCL-38-10056384772-C1.webp" alt="" width="100%">
+                    </div>
+                    <div class="card-content">
+                        <p>Vestido Jeans Feminino Chemise Manga 3/4</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="../images/Vestido-Longo-Feminino-Estampa-Floral-Marisa-10055208154-C1.webp" alt="" width="100%">
+                    </div>
+                    <div class="card-content">
+                        <p>Vestido Longo Feminino Estampa Floral Marisa</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+<?php include 'footer.php'; ?>
+
+</html>
